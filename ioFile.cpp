a038cb197152d87f83a58685d38b0a23176e31e0
Kakonoomoide <rizkiramada128@gmail.com>
2023-06-21 14:51:48 +0700

    membuat input untuk memasukan ke dalam file yang telah di buka

--- ioFile.cpp
+++ ioFile.cpp
@@ -9,4 +9,12 @@ int main(){
 	ofstream outfile;
 	//open file
 	outfile.open("ContohFile.txt");
+
+	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+	while (true){
+		cout << "- ";
+		getline(cin, baris);
+
+	}
 }
