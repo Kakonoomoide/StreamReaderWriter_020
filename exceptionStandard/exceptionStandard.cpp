74b0f766ea330245466f0b7994cb891a76033856
Kakonoomoide <rizkiramada128@gmail.com>
2023-06-21 15:52:21 +0700

    try dan catch jika menggunakan exception dari bahasa pemograman c++

--- exceptionStandard/exceptionStandard.cpp
+++ exceptionStandard/exceptionStandard.cpp
@@ -5,7 +5,13 @@ using namespace std;
 
 int main(){
     cout << "Awal Program" << endl;
-
+    try {
+        array<int, 3> data = { 3,5,7 };/*array dan datanya*/
+        cout << data.at(5) << endl; /*enampilkan array*/
+    }
+    catch (exception& e) {
+        cout << e.what() << endl;/*menampilkan eror dari try*/
+    }
     cout << "Baris program terakhir" << endl;
     return 0;
 }
